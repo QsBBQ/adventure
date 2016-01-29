7e840d62f5202ac2e9de3f7e014b783e8003c045
Quinn Avila <qavila@drw.com>
2016-01-28 18:15:36 -0600

    added fix so app wil not crash when invalid input is given

--- adventure.py
+++ adventure.py
@@ -20,6 +20,10 @@ while True:
         if possible_location:
             print 'to the %s is a %s' % (k, possible_location)
             valid_directions[k] = possible_position
-
-    direction = raw_input('which direction do you want to go?\n')
+    while True:
+        direction = raw_input('which direction do you want to go?\n')
+        if direction in valid_directions.keys():
+            break
+        else:
+            print("Invalid direction! Please choose from the list above")
     position = valid_directions[direction]
